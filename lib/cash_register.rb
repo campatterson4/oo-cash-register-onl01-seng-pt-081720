b383b0ae51cb47947a0b64029cc09200c834da4c
Camryn Patterson <camrynpatterson@yahoo.com>
2020-09-11 19:48:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,38 +1,38 @@
 class CashRegister 
-  attr_accessor :discount, :total
+attr_accessor :discount, :total
 
-  def initialize(discount=0)
-    @discount = discount 
-    @total = 0
-    @items = []
-    @last_item = [] 
-  end 
+def initialize(discount=0)
+@discount = discount 
+@total = 0
+@items = []
+@last_item = [] 
+end 
   
-  def apply_discount
-    if discount != 0 
-      @total -= (@total * (@discount / 100.to_f))
-      "After the discount, the total comes to $#{@total.to_i}."
-    else 
-      "There is no discount to apply."
-    end 
-  end 
+def apply_discount
+if discount != 0 
+@total -= (@total * (@discount / 100.to_f))
+"After the discount, the total comes to $#{@total.to_i}."
+else 
+"There is no discount to apply."
+end 
+end 
   
-  def add_item(title, price, quantity=1)
-    @total += price * quantity 
-    quantity.times do 
-      @items << title 
-    end 
-    @last_item = price * quantity 
-  end 
+def add_item(title, price, quantity=1)
+@total += price * quantity 
+quantity.times do 
+@items << title 
+end
+@last_item = price * quantity 
+end 
   
-  def items 
-    @items 
-  end 
+def items 
+@items 
+end 
   
-  def void_last_transaction 
-     @items.delete_at(-1)
-    self.total = self.total - @last_item
-  end 
+def void_last_transaction 
+@items.delete_at(-1)
+self.total = self.total - @last_item
+end 
 end 
 
 list = CashRegister.new(20) #=>#<CashRegister:0x005616084063b0 @discount=20, @total=0>
